7c3442a41e370d7ba8a2b2a7ac3a3deb055f0df1
Bibazavr <lirik.hariev@gmail.com>
2021-01-11 16:17:58 +0300

    move recursion to last

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,15 @@ import interfaces from './docs/interfaces.md'
 import generics from './docs/generics.md'
 
 function App() {
-    return (
-        <div className="slides r-fit-text" >
-            <section data-markdown={basics} data-separator="^\n\n\n" data-separator-vertical="^\n\n"/>
-            <section data-markdown={recursion} data-separator="^\n\n\n" data-separator-vertical="^\n\n"/>
-            <section data-markdown={interfaces} data-separator="^\n\n\n" data-separator-vertical="^\n\n"/>
-            <section data-markdown={generics} data-separator="^\n\n\n" data-separator-vertical="^\n\n"/>
-        </div>
-    );
+  return (
+    <div className="slides r-fit-text">
+      <section data-markdown={basics} data-separator="^\n\n\n" data-separator-vertical="^\n\n"/>
+      <section data-markdown={interfaces} data-separator="^\n\n\n" data-separator-vertical="^\n\n"/>
+      <section data-markdown={generics} data-separator="^\n\n\n" data-separator-vertical="^\n\n"/>
+      <section data-markdown={recursion} data-separator="^\n\n\n" data-separator-vertical="^\n\n"/>
+
+    </div>
+  );
 }
 
 export default App;
